cf24b95560fd10c09dd3b661b86729aa8dd72d9c
Arshak <arshakbabayan100@gmail.com>
2026-09-08 00:10:42 +0400

    fix

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,17 +2,21 @@ import { Container, Grid } from "@mui/material";
 import Header from "./header/Header";
 import Sidebar from "./sidebar/Sidebar";
 import { Outlet } from "react-router-dom";
+import { useAuth } from "../providers/useAuth";
 
 const Layout = () => {
+  const { user } = useAuth()
   return ( 
     <>
       <Header />
       <Container maxWidth="xl">
         <Grid container spacing={2} sx={{mt: 4}}>
-          <Grid size={3}>
-            <Sidebar />
-          </Grid>
-          <Grid size={9}>
+          {user &&
+            <Grid size={3}>
+              <Sidebar />
+            </Grid>
+          }
+          <Grid size={user ? 9 : 12}>
             <Outlet />
           </Grid>
         </Grid>
